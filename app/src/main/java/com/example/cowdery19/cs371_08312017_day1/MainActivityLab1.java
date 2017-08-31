e970fd0a91d50ffa67f8ba37833d002586017fc8
cowdery19 <cowdery19@up.edu>
2017-08-31 16:47:40 -0700

    Initial commit

---
+++ app/src/main/java/com/example/cowdery19/cs371_08312017_day1/MainActivityLab1.java
@@ -0,0 +1,13 @@
+package com.example.cowdery19.cs371_08312017_day1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityLab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_lab1);
+    }
+}
